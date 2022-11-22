d861d1f72da0bd5a47ced8dd0e255765cc5314db
DESKTOP-GE411VK\julia <julian.benade@gmail.com>
2022-11-22 20:00:44 +0200

    Updated callback Auth URL

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'dev-7epkksmguzfy2ftv.us.auth0.com',
   clientId: '8BntbA4ScsqogIhp68ETVhsDC2ntMX07',
-  callbackUrl: 'http://localhost:3001/callback'
+  callbackUrl: 'https://julianb-git.github.io/serverless-todo/callback'
 }
